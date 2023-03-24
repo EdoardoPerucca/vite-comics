ffb4433a5634ceef9c4a3d70d828aa83353baf1e
EdoardoPerucca <edoardo.perucca@hotmail.com>
2023-03-24 16:36:48 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+
+import HeaderSection from "./components/HeaderSection.vue";
+
+export default {
+  Data() {
+    return {
+
+    }
+  },
+
+  components: {
+    HeaderSection,
+  }
+
+}
+</script>
+
+<template>
+  <div>
+    <HeaderSection></HeaderSection>
+
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    Data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <div id="logo">
+            logo
+        </div>
+
+        <nav>
+            <ul>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+            </ul>
+        </nav>
+
+    </div>
+</template>
+
+<style scoped></style>
